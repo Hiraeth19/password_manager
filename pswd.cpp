0ca70eb74dc8b51f4d54e2ce7ce1cfc7eb50c167
Hiraeth19 <44838351+Hiraeth19@users.noreply.github.com>
2022-01-09 19:24:28 +0530

    Initial commit

---
+++ pswd.cpp
@@ -0,0 +1,97 @@
+# include <iostream>
+# include <fstream>
+# include <string>
+using namespace std;
+
+void error(int e){
+    switch(e){
+        case 1:
+            cout << "There was some error in opening the manager, try again later" << endl;
+            break;
+        case 2:
+             cout << "Sorry the key dosent exist" << endl;
+             break;
+}
+}
+int hashfunc(string key, int mode){
+    int i = key.length();
+    if (mode == 0)      // encryption
+        return i % 19;
+    else
+        return (26 - (i % 19));     // decryption
+}
+string encrypt(string key, string value, int mode){
+    int shift = hashfunc(key, mode);
+    string cipher_txt;
+    for(int x = 0; x < value.length(); x ++){
+       if (isupper(value[x]))
+            cipher_txt += char(int(value[x]+shift-65)%26 +65);
+    // Encrypt Lowercase letters
+        else
+            cipher_txt += char(int(value[x]+shift-97)%26 +97);
+    }
+    return cipher_txt;
+}
+
+void find(string key){
+    string entry;
+    fstream fin;
+    bool found = false;
+    fin.open("cache.txt", ios::in);
+    fin.seekg(0,ios::beg);
+    while(!fin.eof()){
+        getline(fin,entry);
+        string slice1,slice2;
+        int pos = entry.find("-");
+        slice1 = entry.substr(0,pos);
+        if(slice1 == key){
+            slice2 = entry.substr(pos+1);
+            slice2 = encrypt(key,slice2,1);
+            cout << "Password is " << slice2<< endl;
+            found = true;
+        }
+    }
+    if(found == false){
+        error(2);
+    }
+}
+void put(string key, string value){
+    fstream fout;
+    fout.open("cache.txt", ios::out | ios::app);
+    fout.seekp(0,ios::end);
+    fout << key << "-" << value << endl;
+}
+void life(bool status){
+    cout << "Welcome to Password manager" << endl;
+    while(status){
+            cout << "--------------------------------------------" << endl;
+        cout << "Enter 1 for adding a new password" << endl;
+        cout << "Enter 2 for retrieving an old password" << endl;
+        cout << "Enter 3 for exit" << endl;
+        cout << "--------------------------------------------" << endl;
+        int ch;
+        string key,value;
+        cin >> ch;
+        switch(ch){             //TODO: numbers in password 
+            case 1:         // cant add spaces in password
+                cin >> key >> value;
+                value = encrypt(key,value,0);
+                put(key,value);
+                //fout << key << "-" << value << endl;;
+                break;
+            case 2:
+            {
+            cin >> key;
+            find(key);
+            break;
+            }
+        
+            case 5:
+                status = false;
+                break;
+        }
+    }
+}
+int main(){
+    life(true);
+}
